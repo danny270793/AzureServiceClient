238c2df2f61b0ca5fe66f7aa616f9c8a0e68dfcd
Danny Vaca <danny270793@icloud.com>
2024-08-11 15:04:02 -0500

    simplify import way

---
+++ src/responses/index.ts
@@ -0,0 +1,33 @@
+import ActivityRuns from "./activity-runs";
+import DataFlow from "./dataflow";
+import Dataset from "./dataset";
+import IntegrationRuntime from "./integration-runtime";
+import LinkedService from "./linked-service";
+import LoginResponse from "./login-response";
+import PipelineRun from "./pipeline-run";
+import PipelineRuns from "./pipeline-runs";
+import Pipeline from "./pipeline";
+import ResourceGroup from "./resource-group";
+import Resource from "./resource";
+import RoleAssignment from "./role-assigment";
+import SQLPool from "./sql-pool";
+import SQLScript from "./sql-script";
+import Trigger from "./trigger";
+
+export {
+    ActivityRuns,
+    DataFlow,
+    Dataset,
+    IntegrationRuntime,
+    LinkedService,
+    LoginResponse,
+    PipelineRun,
+    PipelineRuns,
+    Pipeline,
+    ResourceGroup,
+    Resource,
+    RoleAssignment,
+    SQLPool,
+    SQLScript,
+    Trigger,
+}
